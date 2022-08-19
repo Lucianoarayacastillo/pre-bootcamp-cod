38a660ee9637d8cfde10fc846fb86cce5eca8f7c
Lucianoarayacastillo <lucianoaraya900@gmail.X-UA-Compatible>
2022-08-19 11:49:58 -0400

    CONFIRMAR

--- index.js
+++
@@ -1,6 +0,0 @@
-function createArray() {        
-        var newArray = [0,1,2,3,4,5];          
-        return newArray;        // agregó la sentencia return
-    }
-    var needAnArray = createArray();        // ahora needAnArray es la variable que llama a createArray
-    
