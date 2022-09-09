b98c44734f53a44072950463320fe925e8131392
Lucianoarayacastillo <lucianoaraya900@gmail.X-UA-Compatible>
2022-09-09 11:57:15 -0300

    confirmarcion

---
+++ JS/araya_luciano_jugueteo.js
@@ -0,0 +1,15 @@
+var alturaniño = 100
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaniño) 
+{
+        if (alturaniño > 56 ){
+        console.log("puedes subir")
+        }else{
+    console.log("para el otro año sera")
+/*
+    Si el alturaniño es mayor a 52
+    , el console.log debe decir "¡Súbete, chico!". 
+    Si no, console.log debe mostrar 
+    "Lo siento, chico. Tal vez el próximo año”
+*/
+    }
+}
